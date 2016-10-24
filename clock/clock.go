0dfda43d5d83a0889c2ec0f46232c80d6c54f08d
Justin Williams <JustinJohnWilliams@gmail.com>
2016-10-24 17:40:18 -0500

    cleanup

--- clock/clock.go
+++ clock/clock.go
@@ -9,8 +9,8 @@ const testVersion = 4
 
 //Clock is a time structure
 type Clock struct {
-	H int
-	M int
+	Hour   int
+	Minute int
 }
 
 //New is the constructor for a Clock
@@ -18,30 +18,30 @@ func New(hour, minute int) Clock {
 	c := Clock{hour, minute}
 
 	if minute >= 60 {
-		c.M = (minute % 60)
-		c.H += (minute / 60)
+		c.Minute = (minute % 60)
+		c.Hour += (minute / 60)
 	}
 
-	if c.H >= 24 {
-		c.H = (c.H % 24)
+	if c.Hour >= 24 {
+		c.Hour = (c.Hour % 24)
 	}
 
-	if c.M < 0 {
-		tmp := int(math.Abs(float64(c.M)))
-		c.M = 60 - (tmp % 60)
+	if c.Minute < 0 {
+		tmp := int(math.Abs(float64(c.Minute)))
+		c.Minute = 60 - (tmp % 60)
 		if tmp > 60 {
-			c.H -= (tmp / 60)
+			c.Hour -= (tmp / 60)
 		}
-		c.H--
+		c.Hour--
 	}
 
-	if c.H < 0 {
-		tmp := int(math.Abs(float64(c.H)))
+	if c.Hour < 0 {
+		tmp := int(math.Abs(float64(c.Hour)))
 		if tmp%24 != 0 {
-			c.H = 24 - (tmp % 24)
+			c.Hour = 24 - (tmp % 24)
 		} else {
 			//we've gone back x days
-			c.H = 0
+			c.Hour = 0
 		}
 	}
 
@@ -49,10 +49,10 @@ func New(hour, minute int) Clock {
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.H, c.M)
+	return fmt.Sprintf("%02d:%02d", c.Hour, c.Minute)
 }
 
 //Add will add minutes to the clock and return the correct time
 func (c Clock) Add(minute int) Clock {
-	return New(c.H, c.M+minute)
+	return New(c.Hour, c.Minute+minute)
 }
