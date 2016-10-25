cbdbd47022672d3bf7ce6d3d3f6eb98b22e429ff
Justin Williams <JustinJohnWilliams@gmail.com>
2016-10-25 08:55:34 -0500

    raindrop solution

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -1,7 +1,27 @@
 package raindrops
 
+import "strconv"
+
 const testVersion = 2
 
-func Convert(int) string
+// Convert will spit out the sound of the rain drop based off of it's facators
+func Convert(rain int) string {
+	var x = ""
+	if rain%3 == 0 {
+		x += "Pling"
+	}
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+	if rain%5 == 0 {
+		x += "Plang"
+	}
+
+	if rain%7 == 0 {
+		x += "Plong"
+	}
+
+	if rain%3 != 0 && rain%5 != 0 && rain%7 != 0 {
+		x = strconv.Itoa(rain)
+	}
+
+	return x
+}
