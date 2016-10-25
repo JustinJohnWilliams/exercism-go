560bce65f12b470f0440ebcba0d5bc2cd8bff356
Justin Williams <JustinJohnWilliams@gmail.com>
2016-10-25 16:14:02 -0500

    triangle done

--- triangle/triangle.go
+++ triangle/triangle.go
@@ -1,17 +1,71 @@
 package triangle
 
-const testVersion = 2
+import "math"
 
-// Code this function.
-func KindFromSides(a, b, c float64) Kind
+const testVersion = 3
 
-// Notice it returns this type.  Pick something suitable.
-type Kind
+// KindFromSides spits out the type of triangle it is from the given side lengths
+func KindFromSides(a, b, c float64) Kind {
+	if !isTriangle(a, b, c) {
+		return NaT
+	}
+
+	if anyZeroes(a, b, c) || anyNaNs(a, b, c) || anyInfs(a, b, c) {
+		return NaT
+	}
+
+	if isIso(a, b, c) {
+		return Iso
+	}
+
+	if a == b && b == c {
+		return Equ
+	}
+
+	return Sca
+}
+
+func isTriangle(a, b, c float64) bool {
+	return ((a + b) >= c) && ((a + c) >= b) && ((b + c) >= a)
+}
+
+func anyZeroes(a, b, c float64) bool {
+	return a <= 0 || b <= 0 || c <= 0
+}
+
+func anyNaNs(a, b, c float64) bool {
+	return math.IsNaN(a) || math.IsNaN(b) || math.IsNaN(c)
+}
+
+func anyInfs(a, b, c float64) bool {
+	return math.IsInf(a, 0) || math.IsInf(b, 0) || math.IsInf(c, 0)
+}
+
+func isIso(a, b, c float64) bool {
+	if a == b && a != c {
+		return true
+	}
+
+	if a == c && a != b {
+		return true
+	}
+
+	if b == c && b != a {
+		return true
+	}
+
+	return false
+}
+
+// Kind = Type of triangle
+type Kind string
 
 // Pick values for the following identifiers used by the test program.
-NaT // not a triangle
-Equ // equilateral
-Iso // isosceles
-Sca // scalene
+const (
+	NaT Kind = "Nat" // not a triangle
+	Equ Kind = "Equ" // equilateral
+	Iso Kind = "Iso" // isosceles
+	Sca Kind = "Sca"
+) // scalene
 
 // Organize your code for readability.
