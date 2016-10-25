6ac674e66352c4d24610f1136554bd469879c864
Justin Williams <JustinJohnWilliams@gmail.com>
2016-10-24 22:53:55 -0500

    hamming bird

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,22 @@
 package hamming
 
+import "errors"
+
 const testVersion = 5
 
+// Distance measures the Hamming distnace between two strands of DNA
 func Distance(a, b string) (int, error) {
-	// your code here!
+	if len(a) != len(b) {
+		return -1, errors.New("string length must be the same")
+	}
+
+	dist := 0
+
+	for i, c := range a {
+		if int(c) != int(b[i]) {
+			dist++
+		}
+	}
+
+	return dist, nil
 }
