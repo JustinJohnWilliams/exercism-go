347ad2c5b95a853d2eed9e89a9822af297504a25
Justin Williams <JustinJohnWilliams@gmail.com>
2016-10-25 14:58:13 -0500

    bob says go

--- bob/bob.go
+++ bob/bob.go
@@ -1,6 +1,5 @@
 package bob // package name must match the package name in bob_test.go
 import (
-	"fmt"
 	"strings"
 	"unicode"
 )
@@ -15,23 +14,23 @@ const (
 
 // Hey is what bob says, the lazy bastard
 func Hey(phrase string) string {
-	fmt.Println(phrase)
-	if phrase == "" || isSpace(phrase) {
+	// fmt.Println(phrase)
+	if isSayingNothing(phrase) {
 		return fine
 	}
 
-	if isYelling(phrase) { //&& strings.Contains(phrase, "!") {
+	if isYelling(phrase) {
 		return chill
 	}
 
-	if strings.Contains(phrase, "?") {
+	if isQuestion(phrase) {
 		return sure
 	}
 
 	return whatever
 }
 
-func isSpace(s string) bool {
+func isSayingNothing(s string) bool {
 	for _, character := range s {
 		if !unicode.IsSpace(character) {
 			return false
@@ -41,10 +40,21 @@ func isSpace(s string) bool {
 }
 
 func isYelling(s string) bool {
+	if !hasLetters(s) {
+		return false
+	}
 	for _, character := range s {
 		if unicode.IsLower(character) && string(character) != "!" && string(character) != "?" {
 			return false
 		}
 	}
 	return true
+}
+
+func isQuestion(s string) bool {
+	return strings.HasSuffix(strings.TrimSpace(s), "?")
+}
+
+func hasLetters(s string) bool {
+	return strings.ContainsAny(s, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 }
